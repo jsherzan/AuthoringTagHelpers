7c6f2fe76b40bc01bf206b4344a76e2c6816098e
jsherzan <77797866+jsherzan@users.noreply.github.com>
2021-04-26 23:27:14 -0500

    Add files via upload

---
+++ TagHelpers/JessTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AuthoringTagHelpers.TagHelpers
+{
+    public class JessTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.Content.SetHtmlContent("This is the Jess button");
+        }
+    }
+}
